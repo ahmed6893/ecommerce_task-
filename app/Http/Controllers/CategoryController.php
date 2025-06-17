d05cf577aa944643365094cbcdebb9edfa0a506a
ahmed6893 <ahmedrifat6893@gmail.com>
2025-06-17 22:25:48 +0600

    Created files and model controller

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $category = Category::all();
+        return view('admin.category.index',['categories'=>$category]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('admin.category.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        Category::saveCategory($request);
+        return back()->with('success','Category Created');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+         Category::updateStatus($id);
+        return back()->with('success','Status Updated');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        return view('admin.category.edit',['category'=>Category::find($id)]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        Category::updateCategory($request ,$id);
+        return back()->with('success','Category Updated Successfully');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        Category::deleteCategory($id);
+        return back()->with('delete','Category deleted Successfully');
+    }
+}
---
+++ app/Models/Category.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    public static $category,$image,$imageName,$imageUrl,$directory,$extension;
+
+    public static function saveCategory($request)
+    {
+        self::$category = new Category();
+        self::$category->name        = $request->name;
+        self::$category->description = $request->description;
+        self::$category->status      = $request->status;
+        self::$category->image       = self::getImageUrl($request);
+        self::$category->save();
+    }
+
+    public static function getImageUrl($request)
+    {
+        self::$image = $request->file('image');
+        self::$extension =self::$image->extension();
+        self::$imageName = rand(000,999).'.'.self::$extension;
+        self::$directory = 'admin/images/category/';
+        self::$imageUrl = self::$directory.self::$imageName;
+        self::$image->move(self::$directory,self::$imageName);
+        return self::$imageUrl;
+    }
+
+    public static function updateCategory($request,$id)
+    {
+        self::$category = Category::find($id);
+        self::$category->name = $request->name;
+        self::$category->description = $request->description;
+        self::$category->status      = $request->status;
+        if ($request->file('image'))
+        {
+            if (file_exists(self::$category->image))
+            {
+                unlink(self::$category->image);
+            }
+            self::$imageUrl=self::getImageUrl($request);
+        }
+        else{
+            self::$imageUrl=self::$category->image;
+        }
+
+        self::$category->save();
+    }
+
+    public static function deleteCategory($id)
+    {
+        self::$category = Category::find($id);
+        if (file_exists(self::$category->image))
+        {
+            unlink(self::$category->image);
+        }
+        self::$category->delete();
+    }
+}
